e9f152b2a57c9f40f884dbeb7bcf9bfac7f74cd0
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-11-07 01:56:44 +0530

    secure the app using ssl certificate

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,9 +1,9 @@
 export default {
 
   oidc: {
-      clientId: '0oa2jbmw08creZo5n5d7',
-      issuer: 'https://dev-48840222.okta.com/oauth2/default',
-      redirectUri: 'http://localhost:4200/login/callback',
+      clientId: '0oa2jclv1cMFjbtt55d7',
+      issuer: 'https://dev-11392778.okta.com/oauth2/default',
+      redirectUri: 'https://localhost:4200/login/callback',
       scopes: ['openid', 'profile', 'email']
   }
 
